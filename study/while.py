6cc4583a2eb85ab11989eaa4724d3d8400c92a09
haneum <gksdma0792@gmail.com>
2026-01-27 18:28:02 +0900

    while문 복습 2

--- study/while.py
+++ study/while.py
@@ -1,7 +1,19 @@
-treehit = 0
-while treehit < 10:
-     treehit = treehit + 1
-     print(f"나무를{treehit}번 찍었습니다.")
-     if treehit == 10:
-         print("나무 넘어갑니다.")
+# treehit = 0
+# while treehit < 10:
+#      treehit = treehit + 1
+#      print(f"나무를{treehit}번 찍었습니다.")
+#      if treehit == 10:
+#          print("나무 넘어갑니다.")
+
+prompt ="""
+1. Add
+2. Del
+3. List
+4. Quit
+
+Enter number: """
+number = 0
+while number !=4:
+    print(prompt)
+    number = int(input())
 
